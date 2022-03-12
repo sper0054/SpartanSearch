a6ddb6a9e3c45167f708f84201054a114061df7e
sper0054 <teresa.sperl@gmail.com>
2022-03-12 10:55:39 -0600

    update

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,17 +3,17 @@ const { Comment } = require('../models');
 const commentdata = [
   {
     comment_text: 'Nunc rhoncus dui vel sem.',
-    user_id: 6,
+    user_id: 1,
     post_id: 1
   },
   {
     comment_text: 'In hac habitasse platea dictumst.',
-    user_id: 7,
+    user_id: 3,
     post_id: 2
   },
   {
     comment_text: 'Vivamus vestibulum sagittis sapien.',
-    user_id: 1,
+    user_id: 2,
     post_id: 3
   },
 ];
