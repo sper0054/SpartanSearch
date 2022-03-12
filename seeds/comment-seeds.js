628683e9f42dd0ac967b34087e155c18a34f2e58
sper0054 <teresa.sperl@gmail.com>
2022-03-12 10:55:07 -0600

    seeds done

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,23 @@
+const { Comment } = require('../models');
+
+const commentdata = [
+  {
+    comment_text: 'Nunc rhoncus dui vel sem.',
+    user_id: 6,
+    post_id: 1
+  },
+  {
+    comment_text: 'In hac habitasse platea dictumst.',
+    user_id: 7,
+    post_id: 2
+  },
+  {
+    comment_text: 'Vivamus vestibulum sagittis sapien.',
+    user_id: 1,
+    post_id: 3
+  },
+];
+
+const seedComments = () => Comment.bulkCreate(commentdata);
+
+module.exports = seedComments;
