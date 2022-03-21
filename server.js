89e80b98a463219709ab0925bdbe3de5ca3c1995
saraneuie <sneuensc@umich.edu>
2022-03-20 23:02:20 -0400

    Add image

--- server.js
+++ server.js
@@ -31,8 +31,13 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static("images"));
 app.use(require('./controllers/'));
+
+// Route to display static src images
+app.get("/static", (req, res) => {
+  res.render("static");
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
